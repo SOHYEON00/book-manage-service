8fa91fc6180e96517538014868bf10fb12719936
오소현 <sohyeon.oh@hnine.com>
2021-04-02 17:40:08 +0900

    feat: 로딩 안내 메시지 출력
    - 도서리스트 불러오기 전 loading... 출력

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import BookListItem from './BookListItem';
 import Table from 'react-bootstrap/Table';
@@ -11,36 +11,46 @@ const theadStyle = {
 };
 
 const BookList = () => {
+    const [isLoading, setIsLoading] = useState(true);
     const list = useSelector(state => state.bookReducer.books);
-    return (<section>
-        <Table hover bordered style={tableStyle}>
-            <thead style={theadStyle}>
-                <tr>
-                    <th>책 제목</th>
-                    <th>저자</th>
-                    <th>출판사</th>
-                    <th>책 형태</th>
-                    <th>대출 상태</th>
-                    <th>대출 날짜</th>
-                </tr>
-            </thead>
-                
-            <tbody>
-               {list.map((item:any) => { 
-                   return (<BookListItem 
-                    key={item.id}
-                    authors={item.authors}
-                    isbn={item.isbn}
-                    publisher={item.publisher}
-                    title={item.title}
-                    isEbook={false}
-                    possibleRent ={true}
-                    rentDate={'yyyy-mm-dd'}
-                   />)})}
-            </tbody>
-        </Table>
-      
-    </section>)
+
+    useEffect(() => {
+        setIsLoading(false);
+    }, [list]);
+
+    return (
+   
+        <section>
+            
+                <Table hover bordered style={tableStyle}>
+                    <thead style={theadStyle}>
+                        <tr>
+                            <th>책 제목</th>
+                            <th>저자</th>
+                            <th>출판사</th>
+                            <th>책 형태</th>
+                            <th>대출 상태</th>
+                            <th>대출 날짜</th>
+                        </tr>
+                    </thead>
+                    { isLoading ? <div> loading ... </div> :     
+                    <tbody>
+                    {list.map((item:any) => { 
+                        return (<BookListItem 
+                            key={item.id}
+                            authors={item.authors}
+                            isbn={item.isbn}
+                            publisher={item.publisher}
+                            title={item.title}
+                            isEbook={false}
+                            possibleRent ={true}
+                            rentDate={'yyyy-mm-dd'}
+                        />)})}
+                    </tbody>  }
+                </Table>
+          
+        </section>
+    );
 };
 
 export default React.memo(BookList);
