7b86e2187f3538ebbb316924a2f92c121ee63438
오소현 <sohyeon.oh@hnine.com>
2021-04-13 14:55:42 +0900

    modify: 구글로그인 관련 코드 정리

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,9 @@
 import * as React from 'react';
 import {useSelector} from 'react-redux';
 import AppRouter from 'components/AppRouter';
-import {mainStyle, titleStyle, errorStyle} from 'styleComponent';
+import {mainStyle, titleStyle} from 'styleComponent';
 import { RootState } from 'modules/reducers';
 import 'style.scss';
-
-//  // DB 도서 추가
-//  const addBook = (bookInfoList:Array<any>) => {
-//   bookInfoList.forEach((bookInfo) => {
-//     dbService.collection('books').add({
-//       authors: bookInfo.authors,
-//       title: bookInfo.title,
-//       publisher: bookInfo.publisher,
-//       isEbook: false,
-//       isRent: false,
-//       borrower: ''
-//     }).then(() => console.log('working well.'))
-//     .catch((error:any) => console.error(`Error : ${error}`));
-//   })   
-// }
-
 
 
 const App = () => {
@@ -30,10 +14,7 @@ const App = () => {
     <>
       <main style={mainStyle}>
         <p style={titleStyle}>도서 관리 서비스</p>
-        {error 
-          ? <div style={errorStyle}>{JSON.stringify(error)}</div> // 에러 메시지 표시
-          : <AppRouter userObj={userObj}/>
-        }
+          <AppRouter userObj={userObj} error={error}/>
       </main>
     </>
   );
